d5bddc899002552e9e5c21324ab571fd0056d0a9
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-28 17:06:03 +0100

    JAVA, ejercicio 20. Completado

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -1,19 +1,37 @@
 package com.campusdual.classroom;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+    //Hay que crear una LISTA de tipo ArrayList
+        Person people1 = new Person("John","Smith");
+        Teacher teacher1= new Teacher("Maria","Montessori","Educación");
+        PoliceOfficer police1= new PoliceOfficer("Jake","Peralta","B-99");
+        Doctor doctor1 = new Doctor("Gregory","House","Nefrologia e infectologia");
 
+        //CREAMOS LA LISTA
+      List<Person> listaPersonas = new ArrayList<>();
+        listaPersonas.add(people1);
+        listaPersonas.add(teacher1);
+        listaPersonas.add(police1);
+        listaPersonas.add(doctor1);
+        return listaPersonas;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
+        for(Person listaPersona : stringList){
+            listaPersona.getDetails();
+        }
+
 
     }
 
     public static void main(String[] args) {
+        //CREAMOS LA LISTA:
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
